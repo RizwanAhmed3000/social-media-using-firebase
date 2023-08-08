bb51ae3f91bce49f379c2955c5e1c7e0b4864dfe
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-08-08 12:58:22 +0500

    followHandler (in progress)

--- friendsPage/friendsPage.js
+++ friendsPage/friendsPage.js
@@ -1,7 +1,19 @@
 const mainContainer = document.querySelector('.mainContainer');
-console.log(mainContainer);
+// console.log(mainContainer);
 
-import {app, auth, db, collection, query, where, getDocs} from "../firebaseConfig.js"
+import { app, auth, db, collection, query, where, getDocs, onAuthStateChanged, updateDoc, doc } from "../firebaseConfig.js"
+
+let loggedinUserId;
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        const uid = user.uid;
+        // getUserData(uid)
+        loggedinUserId = uid
+    } else {
+        window.location.href = '../index.html'
+    }
+});
 
 async function getAllUser() {
     const q = query(collection(db, "users"));
@@ -9,19 +21,17 @@ async function getAllUser() {
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-        const {firstName, lastName, emailAddress} = doc.data();
+        // console.log(doc.id, " => ", doc.data());
+        const { firstName, lastName, emailAddress, profilePicture, description, PhoneNumber} = doc.data();
         const column = document.createElement('div')
-        column.setAttribute('class', 'col');
-        const card = `<div class="card" style="width: 18rem; background-color: #282828; color: #fafafa;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${firstName} ${lastName}</h5>
-            <p class="card-text">add to your friend list. <br/>
-                Email: ${emailAddress}
-            </p>
+        column.setAttribute('class', 'card');
+        const card = ` <div class = "imageContainer"> <img src=${profilePicture || 'https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?w=740&t=st=1685543404~exp=1685544004~hmac=d07ea3ce3ef8f3935685c31c8166ad233839e12607dfb08424f2e5a129f3d691'}  class="card-img-top usersPp" alt="...">
         </div>
-    </div>`
+            <div class="card-body">
+                <h5 class="card-title">${firstName} ${lastName}</h5>
+                <p class="card-text">${description || "No description added"}</p>
+                <button type="button" class="btn btn-outline-warning followBtn" onclick = "followHandler('${firstName}', '${doc.id}', '${lastName}', '${profilePicture}', '${description}', '${emailAddress}', '${PhoneNumber}')">Follow</button>
+            </div>`
         column.innerHTML = card;
         mainContainer.appendChild(column)
     });
@@ -29,3 +39,17 @@ async function getAllUser() {
 }
 
 getAllUser();
+
+async function followHandler(userFirstName, followingUid, userLastName, profilePicture, description, emailAddress, PhoneNumber ){
+    // console.log('working....')
+    console.log(userFirstName, followingUid, userLastName, profilePicture, description, emailAddress, PhoneNumber);
+    console.log(loggedinUserId, "==>> logged in user");
+    const washingtonRef = doc(db, "users", followingUid);
+
+    // Set the "capital" field of the city 'DC'
+    await updateDoc(washingtonRef, {
+        followers: loggedinUserId
+    });
+}
+
+window.followHandler = followHandler;
