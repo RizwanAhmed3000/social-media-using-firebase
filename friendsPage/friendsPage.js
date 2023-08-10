aa973245489961ae5b801aba6d9ad130f078ae9e
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-08-10 11:39:21 +0500

    changing follow text

--- friendsPage/friendsPage.js
+++ friendsPage/friendsPage.js
@@ -45,13 +45,14 @@ async function getUserData(uid) {
 }
 
 async function getAllUser() {
+    mainContainer.innerHTML = ``
     const q = query(collection(db, "users"));
 
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
         // console.log(doc.id, " => ", doc.data());
-        const { firstName, lastName, emailAddress, profilePicture, description, PhoneNumber } = doc.data();
+        const { firstName, lastName, profilePicture, description, followers, following} = doc.data();
         const column = document.createElement('div')
         column.setAttribute('class', 'card');
         const card = ` <div class = "imageContainer"> <img src=${profilePicture || 'https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?w=740&t=st=1685543404~exp=1685544004~hmac=d07ea3ce3ef8f3935685c31c8166ad233839e12607dfb08424f2e5a129f3d691'}  class="card-img-top usersPp" alt="...">
@@ -59,7 +60,7 @@ async function getAllUser() {
             <div class="card-body">
                 <h5 class="card-title">${firstName} ${lastName}</h5>
                 <p class="card-text">${description || "No description added"}</p>
-                <button type="button" class="btn btn-outline-warning followBtn" onclick = "followHandler('${firstName}', '${doc.id}', '${lastName}')">Follow</button>
+                <button type="button" class="btn btn-outline-warning followBtn" onclick = "followHandler('${firstName}', '${doc.id}', '${lastName}')">${followers?.includes(loggedinUserId) ? 'Unfollow' : 'Follow'}</button>
             </div>`
         column.innerHTML = card;
         mainContainer.appendChild(column)
@@ -80,6 +81,7 @@ async function followHandler(userFirstName, followingUid, userLastName) {
         alert(`you are now following ${userFirstName} ${userLastName}`)
         savingFollowersToOtherUser(followingUid, loggedinUserId);
         getUserData(loggedinUserId)
+        getAllUser()
         return
     } else {
         await updateDoc(washingtonRef, {
@@ -88,6 +90,7 @@ async function followHandler(userFirstName, followingUid, userLastName) {
         alert(`you have unfollowed ${userFirstName} ${userLastName}`)
         removingFollowersFromOtherUser(followingUid, loggedinUserId)
         getUserData(loggedinUserId)
+        getAllUser()
     }
 }
 
