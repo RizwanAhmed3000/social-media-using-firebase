c838efbca38fdb63eb2db6c927f102de4ffd1e7e
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-08-15 12:22:44 +0500

    if condition for profile pic

--- editProfile/editProfile.js
+++ editProfile/editProfile.js
@@ -45,71 +45,78 @@ async function getUserData(uid) {
 
 editBtn.addEventListener('click', editProfileHandler);
 
-function editProfileHandler() {
+async function editProfileHandler() {
     console.log(firstNameHtml.value, lastNameHtml.value, description.value, phoneNumHtml.value, profilePicHtml.files[0])
 
     const file = profilePicHtml.files[0];
 
+    if (file) {
+        // Create the file metadata
+        /** @type {any} */
+        const metadata = {
+            contentType: 'image/jpeg'
+        };
 
-    // Create the file metadata
-    /** @type {any} */
-    const metadata = {
-        contentType: 'image/jpeg'
-    };
+        // Upload file and metadata to the object 'images/mountains.jpg'
+        const storageRef = ref(storage, 'profileImages/' + file.name);
+        const uploadTask = uploadBytesResumable(storageRef, file, metadata);
 
-    // Upload file and metadata to the object 'images/mountains.jpg'
-    const storageRef = ref(storage, 'profileImages/' + file.name);
-    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                // A full list of error codes is available at
+                // https://firebase.google.com/docs/storage/web/handle-errors
+                switch (error.code) {
+                    case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+                    case 'storage/canceled':
+                        // User canceled the upload
+                        break;
 
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on('state_changed',
-        (snapshot) => {
-            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-                case 'paused':
-                    console.log('Upload is paused');
-                    break;
-                case 'running':
-                    console.log('Upload is running');
-                    break;
-            }
-        },
-        (error) => {
-            // A full list of error codes is available at
-            // https://firebase.google.com/docs/storage/web/handle-errors
-            switch (error.code) {
-                case 'storage/unauthorized':
-                    // User doesn't have permission to access the object
-                    break;
-                case 'storage/canceled':
-                    // User canceled the upload
-                    break;
+                    // ...
 
-                // ...
-
-                case 'storage/unknown':
-                    // Unknown error occurred, inspect error.serverResponse
-                    break;
-            }
-        },
-        () => {
-            // Upload completed successfully, now we can get the download URL
-            getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-                console.log('File available at', downloadURL);
-                await setDoc(doc(db, "users", loggedinUserId), {
-                    firstName: firstNameHtml.value,
-                    lastName: lastNameHtml.value,
-                    PhoneNumber: phoneNumHtml.value,
-                    profilePicture: downloadURL,
-                    emailAddress: loggedinUserEmail,
-                    description: description.value
+                    case 'storage/unknown':
+                        // Unknown error occurred, inspect error.serverResponse
+                        break;
+                }
+            },
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+                    console.log('File available at', downloadURL);
+                    await setDoc(doc(db, "users", loggedinUserId), {
+                        firstName: firstNameHtml.value,
+                        lastName: lastNameHtml.value,
+                        PhoneNumber: phoneNumHtml.value,
+                        profilePicture: downloadURL,
+                        emailAddress: loggedinUserEmail,
+                        description: description.value
+                    });
                 });
-            });
-
-        }
-    );
-
+            }
+        );
+    } else {
+        await setDoc(doc(db, "users", loggedinUserId), {
+            firstName: firstNameHtml.value,
+            lastName: lastNameHtml.value,
+            PhoneNumber: phoneNumHtml.value,
+            emailAddress: loggedinUserEmail,
+            description: description.value
+        });
+    }
 }
 
